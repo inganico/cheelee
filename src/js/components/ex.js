893bb4f2f9f956cbd0c95758d58efdc9ce70c377
Seltikc <seltikc@yandex.ru>
2023-12-14 20:28:43 +0500

    new project

---
+++ src/js/components/ex.js
@@ -0,0 +1,16 @@
+
+const languages = document.querySelector('.header__languages');
+const en = document.querySelector('.header__languages_en')
+const ru = document.querySelector('.header__languages_ru')
+
+
+en.addEventListener('click', () => {
+  languages.classList.toggle('active')
+})
+
+
+
+
+
+
+
